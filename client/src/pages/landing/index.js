83905411914f7df049503b7ce79a2985d668caf4
ZacWarner <zektgn@gmail.com>
2019-10-07 17:03:23 -0700

    landing page started

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -1,1 +1,18 @@
-import React, { component } from 'react'; 
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <>
+                <div className="jumbotron">
+                    <div className="container text-center">
+                        <h1 className="display-3">Site Name</h1>
+                        <a href="/signup" className="btn btn-primary">Sign up</a>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Landing;
